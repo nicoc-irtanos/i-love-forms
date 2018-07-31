809a735804401217764ee2ec9e3305e8fa11e0d6
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-31 15:53:37 +0200

    comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    # Uncomment pour stocker les données du 1er formulaire
     # User.create(username: params[:username], email: params[:email], bio: params[:bio])
+    # Uncomment pour stocker les données du 2nd formulaire
     # User.create(username: params[:user], email: params[:email], bio: params[:bio])
+    # Commenter les deux lignes au dessus pour stocker les données du 3ème formulaire
     redirect_to('/')
   end
 end
