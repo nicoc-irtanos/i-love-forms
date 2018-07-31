6da7acc63b2152fa521a4dd480ab34a13cef8b7c
nicolasculerier <nicolas.culerier@laposte.net>
2018-07-31 11:08:11 +0200

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'users#home'
   get 'users/new', to: 'users#new', as: 'new'
   post 'users/new', to: 'user#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
